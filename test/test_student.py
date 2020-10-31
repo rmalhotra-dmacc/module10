4eef0f9126ac5f76152d97fce3b07b05a31e3267
Rajiv Malhotra <malhotra.rajiv@principal.com>
2020-10-31 14:12:26 -0500

    student & test_student initial code

--- test/test_student.py
+++ test/test_student.py
@@ -4,14 +4,15 @@ from class_definitions import student as t
 
 class MyTestCase(unittest.TestCase):
     def setUp(self):
-        self.person = t.Student('Duck', 'Daisy', 'English', 4.0)
+        self.student = t.Student('Duck', 'Daisy', 'English', 4.0)
 
     def tearDown(self):
-        del self.person
+        del self.student
 
     def test_initial_value_required_attributes(self):
-        self.assertEqual(self.person.last_name, 'Duck')
-        self.assertEqual(self.person.first_name, 'Daisy')
+        self.assertEqual(self.student.last_name, 'Duck')
+        self.assertEqual(self.student.first_name, 'Daisy')
+        self.assertEqual(self.student.major, 'English')
 
 
 if __name__ == '__main__':
