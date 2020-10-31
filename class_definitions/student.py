0f294fe3ca10f6d1645981fd9fc42859314ad691
Rajiv Malhotra <malhotra.rajiv@principal.com>
2020-10-31 14:41:21 -0500

    Added code & test for test_object_not_created_error_last_name

--- class_definitions/student.py
+++ class_definitions/student.py
@@ -10,6 +10,11 @@ Student Class definition
 class Student:
     """Student class"""
     def __init__(self, lname, fname, major, gpa=0.0):
+        name_characters = set("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'-")
+
+        if not (name_characters.issuperset(lname) and name_characters.issuperset(fname)):
+            raise ValueError
+
         self.last_name = lname
         self.first_name = fname
         self.major = major
@@ -17,3 +22,8 @@ class Student:
 
     def __str__(self):
         return self.last_name + ", " + self.first_name + " has major " + self.major + " with gpa: " + str(self.gpa)
+
+
+if __name__ == '__main__':
+        student = Student('Duck', 'Daisy', 'English', 4.0)
+        print(str(student))
--- test/test_student.py
+++ test/test_student.py
@@ -4,7 +4,7 @@ from class_definitions import student as t
 
 class MyTestCase(unittest.TestCase):
     def setUp(self):
-        self.student = t.Student('Duck', 'Daisy', 'English', 4.0)
+        self.student = t.Student('Duck', 'Daisy', 'English')
 
     def tearDown(self):
         del self.student
@@ -13,15 +13,21 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(self.student.last_name, 'Duck')
         self.assertEqual(self.student.first_name, 'Daisy')
         self.assertEqual(self.student.major, 'English')
+        self.assertEqual(self.student.gpa, 0.0)
 
-    def test_inital_all_attributes(self):
-        self.assertEqual(self.student.last_name, 'Duck')
-        self.assertEqual(self.student.first_name, 'Daisy')
-        self.assertEqual(self.student.major, 'English')
-        self.assertEqual(self.student.gpa, 4.0)
+    def test_initial_all_attributes(self):
+        student = t.Student('Duck', 'Daisy', 'English', 4.0)
+        assert student.last_name == 'Duck'
+        assert student.first_name == 'Daisy'
+        assert student.major == 'English'
+        assert student.gpa == 4.0
 
     def test_student_str(self):
-        self.assertEqual(str(self.student), 'Duck, Daisy has major English with gap: 4.0')
+        self.assertEqual(str(self.student), 'Duck, Daisy has major English with gap: 0.0')
+
+    def test_object_not_created_error_last_name(self):
+        with self.assertRaises(ValueError):
+            p = t.student('123', 'Daisy')
 
 
 if __name__ == '__main__':
